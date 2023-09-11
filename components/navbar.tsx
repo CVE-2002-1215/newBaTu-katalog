86f40d4f5505aa7decdd7ef3a7242926d7472d18
CVE-2002-1215 <cve20021215@gmail.com>
2023-09-12 01:12:04 +0300

    pin navbar top

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -21,7 +21,7 @@ const Navbar = async () => {
   );
   
   return ( 
-    <div className="border-b">
+    <div className="border-b fixed top-0">
       <Container>
         <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
           <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
