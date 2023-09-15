c024f008ba4012a1854e76353f1fbb43d448809f
CVE-2002-1215 <cve20021215@gmail.com>
2023-09-15 18:36:05 +0300

    anasayfa için generateMetadata ile og etiketleri oluşturuldu.

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -7,6 +7,36 @@ import React from 'react';
 
 export const revalidate = 0;
 
+export async function generateMetadata({
+  }) {
+    try {
+        const slides = await getBillboards("e0ec14b4-81fa-4696-8822-eda5cfcc9798"); // Anasayfa billboard
+
+      if(!slides){
+        return {
+          title : "Bulunamadı",
+          description: "Aramakta olduğunuz sayfaya ulaşılamıyor."
+        }
+      }
+      return {
+        openGraph: {
+          title: "New Ba&Tu Hırdavat",
+          description: "Ankara Toptan Hırdavat, Türkiye'nin Tek Adresi | New Ba&Tu Hırdavat",
+          url:`https://www.batuhirdavat.com`,
+          type: 'website',
+          siteName:'batuhirdavat.com',
+          images: slides[0].url,
+        }
+      };
+    } catch(error){
+      console.log(error);
+      return {
+        title : "Bulunamadı",
+        description: "Aramakta olduğunuz sayfaya ulaşılamıyor."
+      }
+    }
+  }
+
 const HomePage: React.FC = async () => {
     const products = await getProducts({isFeatured : true});
     const slides = await getBillboards("e0ec14b4-81fa-4696-8822-eda5cfcc9798"); // Anasayfa billboard
