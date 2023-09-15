d898a8cea4ccbd9143de1d48236857bc1c8fb7e3
CVE-2002-1215 <cve20021215@gmail.com>
2023-09-15 18:23:28 +0300

    category opengraph-image.tsx 'i silince twitter'da hata alıyoruz. Bu sebepten bütün opengraph'leri geri getirdim zararı yok.

---
+++ app/(routes)/opengraph-image .tsx
@@ -0,0 +1,46 @@
+/* eslint-disable @next/next/no-img-element */
+import getBillboards from "@/actions/get-billboards";
+import probe from "probe-image-size";
+import { ImageResponse } from "next/server";
+
+export const alt = "New Ba&Tu Hırdavat";
+export const contentType = "image/png";
+export const size = {
+  width: 1200,
+  height: 630,
+}
+
+export const revalidate = 0;
+
+export default async function og() {
+  
+  const billboards = await getBillboards("e0ec14b4-81fa-4696-8822-eda5cfcc9798");
+  
+  // Get the image dimensions using probe-image-size
+  const { width, height } = await probe(billboards?.[0].url);
+
+  const size = {
+    width,
+    height,
+  };
+
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 48,
+          background: `url(${billboards?.[0].url})`,
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+      </div>
+    ),
+    {
+      ...size
+    }
+  )
+}
